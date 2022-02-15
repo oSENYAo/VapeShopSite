6b09f822026393b45e756a2e70e1a7cdd117eab5
kuv99 <kuv99@mail.ru>
2022-02-15 17:46:04 +0300

    add logging, appsettings move to .gitignore

--- VapeShopSite/Startup.cs
+++ VapeShopSite/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,12 +31,9 @@ namespace VapeShopeSite.WebApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            loggerFactory.CreateLogger<Startup>();
 
             app.UseRouting();
 
